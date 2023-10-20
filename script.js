11c2923e7da9bc231bb3219538c4791198ae4bd4
Meng <mmk070330@gmail.com>
2023-10-20 17:03:29 -0400

    rewrite

--- script.js
+++ script.js
@@ -1,60 +1,56 @@
-// document.querySelector(".number")
 let count = 20;
+let highScore = 0;
 let randomNumber;
-function getRandomInt() {
-    return Math.floor(Math.random() * 20) + 1;
-}
+
+const getRandomInt = () => Math.floor(Math.random() * 20) + 1;
+
+const getNumber = () => document.querySelector(".guess").value;
 
 document.addEventListener('DOMContentLoaded', function() {
     randomNumber = getRandomInt();
     console.log(randomNumber);
 });
 
+function changeContent(selector, newContent){
+    document.querySelector(selector).textContent = newContent;
+    document.querySelector(selector).value = newContent;
+}
+
 function again(){
     randomNumber = getRandomInt();
     console.log("random number is ", randomNumber )
     document.querySelector(".number").textContent = "?"
     count = 20
-    document.querySelector(".score").textContent = `${count}`;
+    changeContent(".score", `${count}`);
+    changeContent(".guess", " ");
+    changeContent(".message", "Start guessing...")
     document.querySelector("body").style.backgroundColor = "black";
-    document.querySelector(".guess").value = " ";
-    document.querySelector(".message").textContent = "Start guessing..."
 }
 
-let highScore = 0;
 function guessNumber(){
-
-    if(count !=0){
-
-        function getNumber(){
-            return document.querySelector(".guess").value;
-        }
-        userInput = getNumber();
-        console.log("The input is: ",userInput);
-        
-        if(userInput <= 20 && userInput >0){
-            count--;
-            if(userInput==randomNumber){
-                count++;
-                highScore = highScore + count;
-                document.querySelector(".number").textContent = `${randomNumber}`
-                document.querySelector(".message").textContent = "Correct! Try again to get higher score."
-                document.querySelector(".highscore").textContent = `${highScore}`
-                document.querySelector("body").style.backgroundColor = "green";
-            }
-            else if(userInput > randomNumber){
-                    document.querySelector(".message").textContent = "too big"
-                }
-            else {
-                document.querySelector(".message").textContent = "too small"
-            }    
-            document.querySelector(".score").textContent = `${count}`;
-        }else{
-     document.querySelector(".message").textContent = "Number between 1-20!."
-        }
+    userInput = getNumber();
+    if(count == 0 ){
+        changeContent(".message", "You Lose!");
+        return;
+    }
+    if(userInput > 20 || userInput <= 0 || userInput == " "){
+        changeContent(".message", "between1-20!")
+        return
+    }
+    count --;
+    if(userInput == randomNumber){
+        count++;
+        highScore = highScore + count;
+        changeContent(".number", `${randomNumber}`);
+        changeContent(".highscore",`${highScore}`);
+        changeContent(".message", "Correct! Try again to get higher score.")         
+        document.querySelector("body").style.backgroundColor = "green";
+    }else if(userInput > randomNumber){
+        changeContent(".message", "too big")
     }else{
-        document.querySelector(".message").textContent = "You lose!"
-    }      
+        changeContent(".message", "too small")
+    }
+    changeContent(".score", `${count}`)    
 }
 
 
